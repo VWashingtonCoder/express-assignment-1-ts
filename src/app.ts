ad53eb1e6516cdcc554d75abbc1986e9baa846cf
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-12-18 11:18:15 -0700

    Set up create endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { prisma } from "../prisma/prisma-instance";
+import { Dog } from "@prisma/client";
 import { errorHandleMiddleware } from "./error-handler";
 import "express-async-errors";
 
@@ -22,7 +23,9 @@ app.get("/dogs/:id", async (req, res) => {
   const { id } = req.params;
 
   if (isNaN(+id)) {
-    return res.status(400).json({ message: "id should be a number" });
+    return res
+      .status(400)
+      .json({ message: "id should be a number" });
   }
 
   const dog = await prisma.dog.findUnique({
@@ -30,10 +33,62 @@ app.get("/dogs/:id", async (req, res) => {
       id: +id,
     },
   });
-  
-  dog 
-    ? res.status(200).json(dog) 
-    : res.status(204).send();
+
+  dog ? res.status(200).json(dog) : res.status(204).send();
+});
+
+// Create endpoint
+app.post("/dogs", async (req, res) => {
+  const body = req.body;
+  const bodyKeys = Object.keys(body);
+  const dogData = {
+    name: body?.name,
+    description: body?.description,
+    breed: body?.breed,
+    age: body?.age,
+  };
+  const { name, description, breed, age } = dogData;
+  const errors = [];
+
+  if (typeof age !== "number") {
+    errors.push("age should be a number");
+  }
+
+  if (typeof name !== "string") {
+    errors.push("name should be a string");
+  }
+
+  if (typeof description !== "string") {
+    errors.push("description should be a string");
+  }
+
+  if (typeof breed !== "string") {
+    errors.push("breed should be a string");
+  }
+
+  bodyKeys.map((key) => {
+    if (
+      key !== "name" &&
+      key !== "description" &&
+      key !== "breed" &&
+      key !== "age"
+    ) {
+      errors.push(`'${key}' is not a valid key`);
+    }
+  });
+
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+
+  try {
+    const dog = await prisma.dog.create({
+      data: dogData,
+    });
+    res.status(201).json(dog);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // all your code should go above this line
